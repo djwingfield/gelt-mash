be6c9fa6423c929812c29eddf182e3c6f12679d8
Dom Wingfield <dom.j.wingfield@gmail.com>
2024-07-16 22:13:10 +0100

    Configure and run prettier

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+// Documentation for this file: https://prettier.io/en/configuration.html
+module.exports = {
+    printWidth: 110,
+    endOfLine: 'auto', // Use .gitattributes to manage newlines
+    singleQuote: true,
+    trailingComma: 'es5',
+    tabs: true,
+    tabWidth: 4,
+};
