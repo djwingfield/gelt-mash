51ee1c2b840003b466c73e712501074fb412e779
Dom Wingfield <dom.j.wingfield@gmail.com>
2024-07-17 12:05:24 +0100

    Add initial tailwind config and app layout

---
+++ apps/gelt-mash/src/components/Header.vue
@@ -0,0 +1,6 @@
+<template>
+    <header class="flex gap-2 align-middle py-4 content-px shadow-header bg-header">
+        <img src="/circle-logo.svg" alt="Menu icon" width="30" height="30" />
+        <h1>Dom Wingfield | MASH!</h1>
+    </header>
+</template>
--- apps/gelt-mash/tailwind.config.js
+++ apps/gelt-mash/tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
     content: ['./index.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                blue: {
+                    25: '#F5FAFF',
+                    100: '#D1E9FF',
+                    500: '#2E90FA',
+                    600: '#1570EF',
+                    700: '#175CD3',
+                    900: '#194185',
+                },
+            },
+            backgroundImage: {
+                header: 'linear-gradient(90deg, #2E90FA 0%, #175CD3 100%)',
+            },
+            boxShadow: {
+                header: '8px 10px 16px 0px rgba(0, 0, 0, 0.2)',
+            },
+        },
     },
     plugins: [],
 };
